1cc0f452126df3def28101995fa6d42d1f18ae3e
Joe Romero <joethemovie@gmail.com>
2017-08-14 15:01:27 -0400

    Testing out Info

---
+++ master-min.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $(".info").click(function() {
+        $(".vehicleInfo").fadeToggle("slow", function() {});
+        if (this.innerHTML === "VEHICLE INFO") {
+            this.innerHTML = "CLOSE INFO";
+        } else {
+            this.innerHTML = "VEHICLE INFO";
+        }
+    });
+});
