e419199a4f9f57dad949dcb824e3f6ab727c0e10
Joe Romero <joethemovie@gmail.com>
2017-08-15 09:57:12 -0400

    added vehicle info

--- master.js
+++ master.js
@@ -9,4 +9,26 @@ $(document).ready(function() {
       this.innerHTML = 'VEHICLE INFO';
     }
   });
+
+  $('.info2').click(function(){
+    $('.vehicleInfo2').fadeToggle('slow', function(){
+
+    });
+    if (this.innerHTML === 'VEHICLE INFO') {
+      this.innerHTML = 'CLOSE INFO';
+    } else {
+      this.innerHTML = 'VEHICLE INFO';
+    }
+  });
+
+  $('.info3').click(function(){
+    $('.vehicleInfo3').fadeToggle('slow', function(){
+
+    });
+    if (this.innerHTML === 'VEHICLE INFO') {
+      this.innerHTML = 'CLOSE INFO';
+    } else {
+      this.innerHTML = 'VEHICLE INFO';
+    }
+  });
 });
